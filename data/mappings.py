7a6be1156da72a934c1c211344f5362d445c23c9
capetromain74-coder <capetromain74@gmail.com>
2026-03-17 11:06:54 +0100

    mappings.py

--- data/mappings.py
+++ data/mappings.py
@@ -7,7 +7,7 @@ MODEL_COLLECTIONS = {
     'jordan-1-mid': ['jordan 1 mid'], 'nike-dunk': ['dunk'], 'air-force-1': ['air force 1'],
     'nike-p-6000': ['air max'], 'nike-vomero': ['vomero'], 'nike-sacail': ['sacai'],
     'adidas-samba': ['samba'], 'adidas-campus': ['campus'], 'adidas-gazelle': ['gazelle'],
-    'adidas-spezial': ['spezial'], 'adidas-forum': ['forum'], 'yeezy-slide': ['yeezy slide'],
+    'adidas-spezial': ['spezial'], 'adidas-forum': ['forum'], 'adidas-superstar': ['superstar'], 'yeezy-slide': ['yeezy slide'],
     'yeezy-351': ['yeezy 350', '350 v2'], 'yeezy-350': ['yeezy 700', '700'],
     'new-balance-550': ['550'], 'new-balance-530': ['530'], 'new-balance-2002r': ['2002r'],
     'new-balance-9060': ['9060'], 'asics-gel-1130': ['gel-1130', 'gel 1130'],
